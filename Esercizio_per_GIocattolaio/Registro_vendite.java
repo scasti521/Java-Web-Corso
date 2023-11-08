bdb09fe68c4e26746b119be6eec3c53def9d2c65
Scasti521 <scastiglione521@gmail.com>
2023-11-08 17:05:03 +0100

    esercizio finale(login-registrazione-acquisto)

---
+++ Esercizio_per_GIocattolaio/Registro_vendite.java
@@ -0,0 +1,9 @@
+package Esercizio_per_GIocattolaio;
+
+public abstract class Registro_vendite {
+    public abstract void aggiungi_vendita();
+    public abstract void rimuovi_vendita();
+    public abstract void visualizza_vendite();
+}
+
+
